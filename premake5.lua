22451f508e3fab6d65aeb2bf46adf4442d5f666b
AEManov20 <AEManov20@codingburgas.bg>
2023-03-21 23:20:21 +0000

    Create premake5 configurations

---
+++ premake5.lua
@@ -0,0 +1,68 @@
+workspace "LittleAlchemyClone"
+   configurations { "Debug", "Release" }
+   architecture "x64"
+
+project "Game"
+   kind "ConsoleApp"
+   language "C++"
+   cppdialect "C++20"
+   staticruntime "On"
+
+   filter "configurations:Debug"
+      targetdir "bin/Debug"
+      objdir "obj/Debug"
+   filter "configurations:Release"
+      targetdir "bin/Release"
+      objdir "obj/Release"
+   filter {}
+   
+   files {
+      "src/*.hpp",
+      "src/*.cpp",
+      "res/icon.rc",
+      
+      -- current system has been configured to work with the raylib implementation
+      -- but whenever there's a new platform just change to the chosen platform's
+      -- source directory
+      "src/Platform/Raylib/*.cpp",
+      "src/Platform/Raylib/*.hpp"
+   }
+
+   includedirs {
+      "vendor/entt/src",
+      "vendor/raylib/include",
+      "vendor/glm",
+      "src"
+   }
+
+   filter "system:windows"
+      links { "raylibdll" }
+      libdirs {
+         "vendor/raylib/lib-windows"
+      }
+   filter "system:linux or system:bsd"
+      links { "raylib", "GL", "m", "pthread", "dl", "rt", "X11" }
+      libdirs {
+         "vendor/raylib/lib-unix"
+      }
+   filter "system:macosx"
+      -- TODO: figure out linker input
+      libdirs {
+         "vendor/raylib/lib-darwin"
+      }
+   filter {}
+   
+   filter "configurations:Debug"
+      postbuildcommands {
+         ("{COPY} vendor/raylib/lib-windows/raylib.dll ./bin/Debug/")
+      }
+      defines { "DEBUG" }
+      symbols "On"
+
+   filter "configurations:Release"
+      postbuildcommands {
+         ("{COPY} vendor/raylib/lib-windows/raylib.dll ./bin/Release/")
+      }
+      defines { "NDEBUG" }
+      optimize "On"
+   filter {}
---
+++ setup.bat
@@ -0,0 +1,2 @@
+@echo off
+.\vendor\premake\windows\premake5.exe vs2022
